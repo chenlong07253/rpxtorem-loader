43c5a9fa89e2d299eaf58b11fd7f2bac6c4c2783
chenlong <904861999@qq.com>
2017-12-20 09:42:21 +0800

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿var loaderUtils = require('loader-utils');
+
+module.exports = function (source, map) {
+    if (this.cacheable) this.cacheable();
+
+    var config = {
+        rem: 20, // 默认 1rem = 20rpx
+        useRem: true, // true:使用rem  false:使用px 1px = 1rpx
+    };
+    var params = loaderUtils.parseQuery(this.query);
+    if (params.rem) config.rem = parseInt(params.rem, 10) || config.rem;
+    if (params.useRem === false) config.useRem = false;
+
+    source = source.replace(/\:\s*([-\d\.]+)rpx/gi, function (a, b) {
+        if (!params.useRem) return ': ' + b + 'px';
+
+        b = parseFloat(b) / config.rem;
+        return ': ' + b + 'rem';
+    });
+
+    this.callback(null, source, map);
+};
