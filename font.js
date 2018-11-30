630487460501ec7f060eac54d2c12d6999127b97
chenlong07253 <904861999@qq.com>
2018-11-30 14:28:55 +0800

    ss

---
+++ font.js
@@ -0,0 +1,12 @@
+!(function () {
+    var remBase = 20; //rem基数
+    function setSize() {
+        var width = document.documentElement.clientWidth;
+        width = width < 320 ? 320 : width > 768 ? 768 : width;
+        var zoom = (width / 375).toFixed(1);
+        document.documentElement.style.fontSize = zoom * remBase + "px";
+    };
+    setSize();
+    window.addEventListener("resize", setSize, false);
+    window.addEventListener("load", setSize, false);
+})();
